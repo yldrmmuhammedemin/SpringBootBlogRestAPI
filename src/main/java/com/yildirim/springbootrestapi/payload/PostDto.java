6aa4d76588cbb47db13dc92f70d22c773ba04f82
Muhammed Emin Yildirim <yldrmmuhammedemin@gmail.com>
2023-03-30 13:17:29 +0300

    PostDto created.

---
+++ src/main/java/com/yildirim/springbootrestapi/payload/PostDto.java
@@ -0,0 +1,11 @@
+package com.yildirim.springbootrestapi.payload;
+
+import lombok.Data;
+
+@Data
+public class PostDto {
+    private long id;
+    private String title;
+    private String description;
+    private String content;
+}
