f5f89b9f9d640dae6019f7026c5298a963da5796
Muhammed Emin Yildirim <yldrmmuhammedemin@gmail.com>
2023-03-30 13:14:20 +0300

    Post entity and custom runtime exception created.

---
+++ src/main/java/com/yildirim/springbootrestapi/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.yildirim.springbootrestapi.repository;
+
+import com.yildirim.springbootrestapi.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
